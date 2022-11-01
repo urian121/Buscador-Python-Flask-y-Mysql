62bf881b606c4a49adf420f866a42dd7152511b4
urian <urian1213viera@gmail.com>
2022-10-31 19:27:15 -0500

    ajustes pequeños

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 #Importando  flask y algunos paquetes
 from flask import Flask, render_template, request, redirect, url_for
-import time
 from confiDB import *  #Importando conexion BD
 
 
@@ -9,7 +8,6 @@ app = Flask(__name__)
 application = app
 
 app.secret_key = '97110c78ae51a45afcb3380af008f90b23a5d1616bf19bc29098105da20fe'
-
 
 
 #Creando mi Decorador para el Home
@@ -29,7 +27,6 @@ def BuscarEmpleado():
         resultadoBusqueda = cur.fetchone()  
         cur.close() #Cerrando conexion SQL
         conexion_MySQLdb.close() #cerrando conexion de la BD
-        time.sleep(0.5) #tiempo de espera
         return render_template('public/resultadoBusqueda.html', miData = resultadoBusqueda, busqueda = search)
     return redirect(url_for('inicio'))  
    
